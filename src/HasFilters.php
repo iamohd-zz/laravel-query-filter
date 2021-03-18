ff0ed9e9a120206f7c607f79ffc9581f857ed68c
Mohammed Isa <mohd.itcs@gmail.com>
2021-03-18 16:20:55 +0300

    Ability to passing the filter class name to filter method

--- src/HasFilters.php
+++ src/HasFilters.php
@@ -10,11 +10,15 @@ trait HasFilters
      * Filter a result set.
      *
      * @param \Illuminate\Database\Eloquent\Builder $query
-     * @param \Smartisan\QueryFilter\QueryFilter $filters
+     * @param \Smartisan\QueryFilter\QueryFilter|string $filters
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeFilter(Builder $query, QueryFilter $filters): Builder
+    public function scopeFilter(Builder $query, $filters): Builder
     {
+        if (is_string($filters)) {
+            $filters = app($filters);
+        }
+
         return $filters->apply($query);
     }
 }
